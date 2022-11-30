99d1d1ad6fafefd68385e8be2bcce9aaaffdc5f6
nitish2099 <nitishnivedan>
2022-11-30 18:30:04 +0530

    5th Assignment: Added Search Functionality for our Team G - Gladiators

--- src/Assignment4/CardComponent/index.js
+++ src/Assignment4/CardComponent/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import userIcon from "../../logo/userIcon.png";
 import "./styles-card.css";
 
 const CardComponent = ({ temmatesDetails }) => (
   <div className="CardWrapper">
-    <img src={temmatesDetails.imageSource} className="imageClass" />{" "}
+    <img
+      src={temmatesDetails.imageSource}
+      className="imageClass"
+      alt={userIcon}
+    />{" "}
     <div className="information-class">Name : {temmatesDetails.name}</div>
     <div className="information-class">Place: {temmatesDetails.location}</div>
     <div className="information-class">Company: {temmatesDetails.company}</div>
---
+++ src/Assignment4/SearchComponent/index.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+
+import { useState } from "react";
+
+const SearchComponent = ({ setCurrentCards, teamInformation }) => {
+  const [nameOrDesignation, setNameOrDesignation] = useState("");
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        setCurrentCards(
+          teamInformation.filter(
+            (item) =>
+              item.name
+                .toUpperCase()
+                .includes(nameOrDesignation.toUpperCase()) ||
+              item.designaton
+                .toUpperCase()
+                .includes(nameOrDesignation.toUpperCase())
+          )
+        );
+      }}
+    >
+      <div className="searchClass">
+        <input
+          type="text"
+          value={nameOrDesignation}
+          onChange={(event) => setNameOrDesignation(event.target.value)}
+        />
+        <button className="buttonClass">Search</button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchComponent;
