fe4b0b8dbddfa8b641f361e566f6250e7d51b1c8
nitish2099 <nitishnivedan>
2022-12-13 02:26:40 +0530

    Assignment 8: using Class Components and consoling the sequence of Lifecycle methods called

---
+++ src/Assignment4/AboutMe/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+class AboutMe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teamName: "Gladiators",
+      componentType: "Class Component",
+    };
+    console.log("constructor called of Parent");
+  }
+
+  componentDidMount() {
+    console.log("Component Did Mount called of Parent");
+  }
+
+  componentDidUpdate() {
+    console.log("Component Did Update of Parent");
+  }
+
+  render() {
+    const { teamName, componentType } = this.state;
+    console.log("render called of Parent");
+    return (
+      <>
+        <h1>This page is about {teamName}</h1>
+        <h2>Page is made using {componentType}</h2>
+        <Outlet />
+      </>
+    );
+  }
+}
+export default AboutMe;
